6b06f81df65ecb5e40a22093eed8886701f2dd85
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-06-14 14:16:35 +0500

    qwer

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>Home
+        asdkadsnaks
+        askldnalsda
+        a,mnsdknasd
+        ,aksdnklasd
+        
+    </div>
+  )
+}
+
+export default Home
