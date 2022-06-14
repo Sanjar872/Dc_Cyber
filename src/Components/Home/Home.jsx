015cd1918b30fe02bada1c0f51e5858ee5b8bdc6
farohiddin_M <farohiddinmamarasulov@gmail.com>
2022-06-14 14:43:01 +0500

    aka HTML bi ochirvordim

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,17 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>Home
-        asdkadsnaks
-        askldnalsda
-        a,mnsdknasdm,nc,bxvnc,v .c,mv,
-        ,aksdnklasdasdasdasdasd
-qwqwqwdqwasdasd
-asdasd
-<h5>kl;asfklasdlasjfksjfkjjskkld kasjdlasdhasdhbaidaisdnja</h5>
-asdasdasd
-
-        <h2>Sanjar aka nima gap tinchmi</h2>
-<h1>asdasdasda</h1>
+  
     </div>
   )
 }
