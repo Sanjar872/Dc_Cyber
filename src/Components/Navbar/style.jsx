e2ccc8bc97c99a0cd945088bf83669001b36206d
farohiddin_M <farohiddinmamarasulov@gmail.com>
2022-06-14 16:28:15 +0500

    Aka men ozgartrdim ozmi Navbarimni

--- src/Components/Navbar/style.jsx
+++ src/Components/Navbar/style.jsx
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
+//bgc img
+import Bgcimg from './../F-image/Navbar-bgc-img.png'
+
 export const MainDiv = styled.div`
     width: 100%;
     height: 100vh;
     border: 1px solid red;
+    background-image: url(${Bgcimg});
+    background-size: cover;
 `
+
