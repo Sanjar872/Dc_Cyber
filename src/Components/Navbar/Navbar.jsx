b5f629c43393b1700105abfcfb175e4850044d3c
farohiddin_M <farohiddinmamarasulov@gmail.com>
2022-06-15 17:21:41 +0500

    aka men navbarni tugatmadim

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import { MainDiv } from './style'
+import { Contener, Hamburger, LI, LogoDiv, LogoImg, MainDiv, MuiButton, NavbarDiv, UL } from './style'
+
+
+//img
+import LogotipImg from './../F-image/Logotip-img.png'
 
 const Navbar = () => {
   return (
-    <MainDiv>Sanjar aka </MainDiv>
+    <MainDiv>
+      <NavbarDiv>
+        <LogoDiv>
+          <LogoImg src={LogotipImg} />
+        </LogoDiv>
+        <Contener>
+        <UL>
+          <LI>О нас</LI>
+          <LI>Турниры</LI>
+          <LI>Счёты</LI>
+          <LI>Фото</LI>
+        </UL>
+          <MuiButton variant='contained' >Регистрация</MuiButton>
+        </Contener>
+      </NavbarDiv>
+    </MainDiv>
   )
 }
 
--- src/Components/Navbar/style.jsx
+++ src/Components/Navbar/style.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import HamburgerBars from './MenuBars/MenuBars'
+
+// import mui
+import Button from '@mui/material/Button';
 
 //bgc img
 import Bgcimg from './../F-image/Navbar-bgc-img.png'
@@ -10,4 +14,71 @@ export const MainDiv = styled.div`
     background-image: url(${Bgcimg});
     background-size: cover;
 `
+
+export const NavbarDiv = styled.div`
+    width: 100%;
+    height: 100px;
+    border: 1px solid white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 85px;
+
+    @media screen and (max-width:1044px) {
+        padding: 0px 40px;
+    }
+`
+
+export const LogoDiv = styled.div`
+    width: 195px;
+    height: 45px;
+    border: 1px solid yellow;
+    
+`
+
+export const LogoImg = styled.img`
+    width: 100%;
+    height: 100%;
+`
+
+export const Contener = styled.div`
+    width: 550px;
+    height: 80px;
+    border: 1px solid gray;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    
+    @media screen and (max-width:835px) {
+        display:none ;
+    }
+`
+
+export const UL = styled.ul`
+    width: 380px;
+    height: 70px;
+    border: 1px solid blue;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`
+
+export const LI = styled.li`
+    width: 80px;
+    height: 30px;
+    border: 1px solid blueviolet;
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 500;
+    font-size: 16px;
+    color: white;
+`
+
+export const MuiButton = styled(Button)`
+    
+`
+
 
