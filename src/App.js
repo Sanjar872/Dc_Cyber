b0a253b94c429717d30fdbcb4f2c4caec38f81d4
Sanjar872 <Sanjarbek.muxiddinov@gmail.com>
2022-06-21 13:21:22 +0500

    nima gapa

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import './App.css'
 //components
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Components/Home/Home'
-<<<<<<< HEAD
+import запис from './Components/Записатся/запис'
 import Tournament from './Components/Tournament/Tournament'
 
-=======
-import запис from './Components/Записатся/запис'
->>>>>>> 1436cb10cc2466eff6e488a5a0d8f4f9a318baeb
 const App = () => {
   return (
     <>
     <Navbar />
     <Home/>
-<<<<<<< HEAD
-    <Tournament />
-=======
     <запис/>
->>>>>>> 1436cb10cc2466eff6e488a5a0d8f4f9a318baeb
+    <Tournament />
+
+
     </>
   )
 }
